f82972603f94137d5b14e39f2c9d19e6f295d417
Shekh Ahammed Adnan Bashir <0905107.saab@ugrad.cse.buet.ac.bd>
2019-12-16 11:21:38 +0600

    Fixed issues with structures added for multiprocessing.

--- project_configs.py
+++ project_configs.py
@@ -1,1 +1,2 @@
 DATASET_ROOT = '/home/adnan/CSE 6411 Materials/Dataset/'
+TEST_DATASET_ROOT = '/home/adnan/PycharmProjects/BUET-CSE-6411-project/data/test_dataset'
--- src/datasets/compile_neighbor_rank_histogram_dataset.py
+++ src/datasets/compile_neighbor_rank_histogram_dataset.py
@@ -6,7 +6,7 @@ import h5py
 import numpy as np
 from random import sample as random_sample
 
-from project_configs import DATASET_ROOT
+from project_configs import DATASET_ROOT, TEST_DATASET_ROOT
 from src.features.neighbor_rank_histogram import NeighborRankHistogram
 
 
@@ -15,7 +15,7 @@ class CompileNeighborRankHistogram:
         self.dataset_root_path = dataset_root_path
         self.neighbor_rank_histogram = NeighborRankHistogram(dataset_root_path, K, bases, right_neighbor_count, left_neighbor_count)
 
-    def compile_entire_dataset(self, write_path, subsets=None, subset_fractions=None) -> None:
+    def compile_entire_dataset(self, write_path=None, subsets=None, subset_fractions=None):
         """
         Writes the entire dataset in the destination defined in the write_path.
         :param write_path: Absolute path to the writing destination.
@@ -35,19 +35,28 @@ class CompileNeighborRankHistogram:
             if dataset_dir in subsets:
                 label = int(dataset_dir[-1])
                 end_index = floor(subset_fraction*len(os.listdir(os.path.join(self.dataset_root_path, dataset_dir))))
-                dataset_dir = dataset_dir[:end_index]
-                vectors, labels = self.compile_dataset(dataset_dir, label)
+                vectors, labels = self.compile_dataset(dataset_dir, label, end_index=end_index)
                 X.extend(vectors)
                 y.extend(labels)
-        X, y = zip(*random_sample(list(zip(X, y)), len(X)))
 
-        hf = h5py.File(write_path, 'w')
-        hf.create_dataset('X', data=np.array(X))
-        hf.create_dataset('y', data=np.array(y))
+        if len(subsets) > 1:
+            X, y = zip(*random_sample(list(zip(X, y)), len(X)))
 
-    def compile_dataset(self, dataset_dir, label, write_path=None):
+        if write_path is not None:
+            hf = h5py.File(write_path, 'w')
+            hf.create_dataset('X', data=np.array(X))
+            hf.create_dataset('y', data=np.array(y))
+        else:
+            return X, y
+
+    def compile_dataset(self, dataset_dir, label, write_path=None, end_index=None):
         fasta_dir_path = os.path.join(self.dataset_root_path, dataset_dir)
         fasta_file_paths = [os.path.join(fasta_dir_path, fasta_file) for fasta_file in os.listdir(fasta_dir_path)]
+
+        if end_index is None:
+            fasta_file_paths = fasta_file_paths[:len(fasta_file_paths)]
+        else:
+            fasta_file_paths = fasta_file_paths[:end_index]
 
         X = []
         for count, fasta_file_path in enumerate(fasta_file_paths):
@@ -74,12 +83,12 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_root_path', type=str, default=DATASET_ROOT)
+    parser.add_argument('--dataset_root_path', type=str, default=TEST_DATASET_ROOT)
     parser.add_argument('--K', type=int, default=3)
     parser.add_argument('--bases', type=str, default='ACGT')
     parser.add_argument('--right_neighbor_count', type=int, default=3)
     parser.add_argument('--left_neighbor_count', type=int, default=3)
-    parser.add_argument('--write_path', type=str, default='/home/adnan/PycharmProjects/BUET-CSE-6411-project/data/neighbor_rank_histogram_dataset_imbalanced.h5')
+    parser.add_argument('--write_path', type=str, default='/home/adnan/PycharmProjects/BUET-CSE-6411-project/data/test_neighbor_rank_histogram_dataset_imbalanced.h5')
     parser_args = parser.parse_args()
 
     main(parser_args)
